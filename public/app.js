22042c96cfc1996e9dd9743ba3394f76ab5f9a77
Balazs Cseh <balazs.cseh89@gmail.com>
2015-01-03 16:08:02 +0000

    done basic angular setup, no actual function or calls yet

---
+++ public/app.js
@@ -0,0 +1,7 @@
+var GpsKovetoApp = angular.module('GpsKovetoApp', ['ngRoute'])
+.config(['$routeProvider', '$locationProvider', '$httpProvider',
+	function($routeProvider, $locationProvider, $httpProvider) {
+
+
+
+}]);
