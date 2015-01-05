3c536a224f94e95b00519228c556874fa92292db
Balazs Cseh <balazs.cseh89@gmail.com>
2015-01-05 22:30:14 +0000

    added first controller and partial, took map off landing pg

--- public/app.js
+++ public/app.js
@@ -2,16 +2,16 @@ var GpsKovetoApp = angular.module('GpsKovetoApp', ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', '$httpProvider',
 	function($routeProvider, $locationProvider, $httpProvider) {
 
-	  $routeProvider
-	    .when('/login', {
-	title: 'Login',
-	templateUrl: 'html/login.html',
-	controller: 'LoginController',
-	controllerAs: 'loginvctrl',
-	access: {requireLogin: false}
-	    });
+		$routeProvider
+			.when('/admin', {
+		title: 'Admin',
+		templateUrl: 'html/admin.html',
+		controller: 'AdminController',
+		controllerAs: 'adminvctrl',
+		// access: {requireLogin: false}
+		    });
 
-	$locationProvider.html5Mode(true);
+		$locationProvider.html5Mode(true);
 }]);
 
 
---
+++ public/controllers/admin.js
@@ -0,0 +1,5 @@
+angular.module('GpsKovetoApp')
+
+.controller('AdminController', [function() {
+	
+}]);
