e007f507408d144f20bcf1b20004dfdf3045fd85
Balazs Cseh <balazs.cseh89@gmail.com>
2015-07-19 23:51:48 +0100

    created all html files and skeleton for reporting view

--- public/js/controllers/reporting.js
+++ public/js/controllers/reporting.js
@@ -2,4 +2,9 @@ angular.module('GpsKovetoApp')
 
 .controller('ReportingController', ['$scope', function($scope) {
 
+    $scope.selected = 'tracker_config';
+
+    $scope.setSelected = function(value) {
+        $scope.selected = value;
+    };
 }]);
