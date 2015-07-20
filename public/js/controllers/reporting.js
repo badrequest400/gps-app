b1ece0ec1b8a85b830c7207b38418aa89648a95a
Balazs Cseh <balazs.cseh89@gmail.com>
2015-07-20 23:48:53 +0100

    did quick date picker directive, not completely right yet, started gps report view

--- public/js/controllers/reporting.js
+++ public/js/controllers/reporting.js
@@ -3,8 +3,12 @@ angular.module('GpsKovetoApp')
 .controller('ReportingController', ['$scope', function($scope) {
 
     $scope.selected = 'tracker_config';
+    $scope.filter = {};
 
     $scope.setSelected = function(value) {
         $scope.selected = value;
     };
+
+    console.log($scope.filter.from)
+    console.log($scope.filter.to)
 }]);
--- public/js/directives/quick_date.js
+++ public/js/directives/quick_date.js
@@ -1,39 +1,86 @@
 angular.module('GpsKovetoApp')
 
+// GENERATE FROM-TO DATE OBJECTS FOR FILTERING QUERIES
+// ACCORDING TO QUICK TIME FILTER VALUES
+
+
+
+//////////////////////////////////////////////////
+
+// DATE VALUES ARE NOT RIGHT YET, TEST, TEST, TEST!!!!
+
+
+/////////////////////////////////////////////////
+
+
+
 .directive('quickDate', function() {
 
     return {
         restrict: 'A',
         link: function(scope, element, attributes) {
+            
+            // DEFAULT --> YESTERDAY
+            var date = new Date();
+            date.setDate(date.getDate()-1);
+            scope.filter.from = timeNull(date);
+            scope.filter.to = timeNull(new Date());
 
             element.on('change', function() {
                 switch (element.val()) {
                     case "yesterday":
                         var date = new Date();
-                        var yesterday = date.setDate(date.getDate()-1)
-                        scope.filter.from = timeNull(yesterday);
-                        scope.filter.to = timeNull(date);
+                        date.setDate(date.getDate()-1);
+                        scope.filter.from = timeNull(date);
+                        scope.filter.to = timeNull(new Date());
                         break;
                     case "last_7_days":
-
+                        var date = new Date();
+                        date.setDate(date.getDate() -7);
+                        scope.filter.from = date
+                        scope.filter.to = new Date();
                         break;
                     case "last_30_days":
-
+                        var date = new Date();
+                        date.setDate(date.getDate() -30);
+                        scope.filter.from = date
+                        scope.filter.to = new Date();
                         break;
                     case "last_week":
-
+                        var date = new Date();
+                        date.setDate(date.getDate() -7);
+                        date.setDate(date.getDate() - date.getDay() + 1);
+                        var date2 = new Date();
+                        date2.setDate(date.getDate() - date.getDay());
+                        scope.filter.from = date;
+                        scope.filter.to = date2;
                         break;
                     case "current_week":
-
+                        var date = new Date();
+                        date.setDate(date.getDate() - date.getDay() + 1);
+                        scope.filter.from = timeNull(date);
+                        scope.filter.to = new Date();
                         break;
                     case "last_month":
-
+                        var date = new Date();
+                        date.setDate(date.getDate() - 30);
+                        date.setDate(1);
+                        var date2 = new Date();
+                        date2.setDate(date2.getDate() - 30);
+                        date2.setDate(30);
+                        date2.setHours(23);
+                        date2.setMinutes(59);
+                        scope.filter.from = timeNull(date);
+                        scope.filter.to = date2;
                         break;
                     case "current_month":
-
+                        var date = new Date();
+                        date.setDate(1);
+                        scope.filter.from = timeNull(date);
+                        scope.filter.to = new Date();
                         break;
                     case "custom":
-
+                        // EMBED CUSTOM DATE-TIME PICKER
                         break;
                 };
             });
